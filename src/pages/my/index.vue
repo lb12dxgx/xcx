b2075b4a6e1d9a4e05f0527ae201294b12927df5
lb1213 <zhanggy@weeon.cn>
2018-08-29 15:37:38 +0800

    完善注册

--- src/pages/my/index.vue
+++ src/pages/my/index.vue
@@ -9,7 +9,7 @@
          {{nickName}}
        </div> 
     </div>
-     <div class="weui-cells weui-cells_form">
+     <div class="weui-cells weui-cells_form" v-if="isLogin">
        <div class="weui-cell ">
                   <div class="weui-cell__hd">
                       <label class="weui-label">手机号</label>
@@ -27,7 +27,7 @@
                 <input class="weui-input" type="number" pattern="[0-9]*" placeholder="请输入验证码" v-model="smscode">
             </div>
             <div class="weui-cell__ft">
-                <a  class="weui-btn weui-btn_mini weui-btn_primary" @click="sendSms" >发送验证码</a>
+                <a  class="weui-btn weui-btn_mini weui-btn_primary" @click="sendSms"  >{{sendMsg}}</a>
             </div>  
       </div>
       <div class="weui-cell ">
@@ -41,7 +41,10 @@
       </div>
     </div>
          
-    <a  class="weui-btn weui-btn_min weui-btn_primary" style="width:70%;margin-top:20px" @click="regUser">登录</a> 
+    <a  class="weui-btn weui-btn_min weui-btn_primary" style="width:70%;margin-top:20px" @click="regUser" v-if="isLogin">登录</a> 
+    <a  class="weui-btn weui-btn_min weui-btn_primary" style="width:70%;margin-top:20px" @click="logOut" v-if="!isLogin">注销</a> 
+   
+   
     
     </div>
  
@@ -53,16 +56,18 @@ import {isExitByTelphone,getSmsCode,validateSmsCode,getOpenId,regUser} from '../
 export default {
   data () {
     return {
-      openid:'',
+      senddis:false,
+      isLogin:true,
+      openId:'',
       avatarUrl:'',
       nickName:'',
 
       telphone:'13720053036',
       smscode:'2688',
       name:'张三',
-      
       regInfo:{},
-      
+      sendMsg:'发送验证码'
+    
     }
      
   },
@@ -75,7 +80,7 @@ export default {
       wx.login({
         success: (r) => {
           getOpenId({"code":r.code}).then((res) => {
-            this.openid=res.retData.openid;
+            this.openId=res.retData.openid;
             console.log('res.retData==='+res.retData)
               wx.getUserInfo({
                 success: (res) => {
@@ -90,15 +95,19 @@ export default {
       })
     },
     sendSms(){
-      
+       if(this.senddis){
+       	 return false;
+       };
+
        var myreg=/^[1][3,4,5,7,8][0-9]{9}$/;
+       
        if(!myreg.test(this.telphone)||this.telphone==''){
           wx.showToast({
             title: '手机号格式不对',
             duration: 2000
           });
           return false;
-       }
+       };
 
       isExitByTelphone({'telphone':this.telphone,"type":0}).then((res) => {
         
@@ -108,11 +117,34 @@ export default {
             duration: 2000
           }) 
         }else{
-          getSmsCode({'telphone':this.telphone,'openId':this.openId,"type":0}).then((res) => {
-            this.regInfo=res.retData;
+          getSmsCode({'realName':this.name,'telphone':this.telphone,'openId':this.openId,"type":0}).then((res) => {
+              this.senddis=true;
+              if(res.state==1){
+              	wx.showToast({
+		            title: '短信已经发送',
+		            duration: 2000
+          		}) 
+              }
+
+               var m=60;
+               var interval=setInterval(()=>{
+	              m=m-1;
+	              this.sendMsg=m+"秒";
+	              if(m<=0){
+	                 clearInterval(interval);
+	                 this.senddis=false;
+	                 this.sendMsg="再次发送"
+	                 return; 
+	              }
+	             },1000);
           })
         }
       })
+    },
+
+    logOut(){
+    	wx.clearStorageSync();
+    	this.isLogin=true;
     },
 
     regUser(){
@@ -142,24 +174,25 @@ export default {
             duration: 2000
           }) 
         }else{
-          this.regInfo.telphone=this.telphone;
+         this.regInfo.telphone=this.telphone;
          this.regInfo.userName=this.telphone;
          this.regInfo.openId=this.openId;
          this.regInfo.avatarUrl=this.avatarUrl;
          this.regInfo.nickName=this.nickName;
+         this.regInfo.smscode=this.smscode;
+         
 
          regUser(this.regInfo) .then((res)=>{
-            this.regInfo=res.retData;
-            var regInfostr=JSON.stringify(this.regInfo);
-            console.log(regInfostr);
-            wx.setStorageSync({
-              key:"regInfo ",
-              data:regInfostr
-            });
-
-            wx.switchTab({
+            wx.setStorageSync("openId",this.openId);
+            wx.setStorageSync("avatarUrl",this.avatarUrl);
+            wx.setStorageSync("nickName",this.nickName);
+            console.log(wx.getStorageSync("avatarUrl"));
+           wx.switchTab({
               url: '/pages/index/main'
             })
+            /*wx.switchTab({
+              url: '/pages/index/main'
+            })*/
            
          })
        }
@@ -167,14 +200,14 @@ export default {
     })
   },
 },
-   onShow() {
-    console.log(wx.getStorageSync('regInfo'));
-    if(wx.getStorageSync('regInfo')!=''){
-         var regInfostr=wx.getStorageSync('regInfo');
-         this.regInfo=JSON.parse(regInfostr);
-         this.avatarUrl = this.regInfo.avatarUrl;
-         this.nickName = this.regInfo.nickName;
-         this.openid=this.regInfo.openid;
+  onShow() {
+    console.log(wx.getStorageSync("openId"));
+    if(wx.getStorageSync("openId")!=''){
+    	  this.isLogin=false;
+          this.openid=wx.getStorageSync('openId');
+          this.avatarUrl = wx.getStorageSync('avatarUrl');
+          this.nickName = wx.getStorageSync('nickName');
+         
     }else{
         this.getUserInfo();
     }
